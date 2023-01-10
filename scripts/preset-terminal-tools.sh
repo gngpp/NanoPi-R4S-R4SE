a44c377850210514aad2b98350eec9af7dc82aaa
relife <verticle@foxmail.com>
2023-01-10 14:31:51 +0800

    update

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -20,7 +20,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 
 # Get .zshrc dotfile
-cp ../scripts/.zshrc .
-cp ../scripts/update.sh .
+cp ../../scripts/.zshrc .
+cp ../../scripts/update.sh .
 
 popd
