f7218221ad3c988174fd301eb2bf651ff4999b6c
relife <verticle@foxmail.com>
2023-01-11 12:48:01 +0800

    update

--- scripts/feeds_settings.sh
+++ scripts/feeds_settings.sh
@@ -8,6 +8,8 @@ pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
 sed -i '/src-git packages/d' feeds.conf.default
+sed -i '/src-link packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
+sed -i '/src-link luci/d' feeds.conf.default 
 echo "src-link luci $luci_feed" >> feeds.conf.default
