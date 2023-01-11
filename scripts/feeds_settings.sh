169bf17ae6c9348682126a7860a74f5487cb294b
gngpp <gngppz@gmail.com>
2023-01-11 14:53:21 +0000

    update

--- scripts/feeds_settings.sh
+++ scripts/feeds_settings.sh
@@ -14,3 +14,24 @@ sed -i '/src-git luci/d' feeds.conf.default
 sed -i '/src-link luci/d' feeds.conf.default 
 echo "src-link luci $luci_feed" >> feeds.conf.default
 cat feeds.conf.default
+
+max_iteration=5
+
+for i in $(seq 1 $max_iteration)
+do
+  ./scripts/feeds update -a && ./scripts/feeds install -a
+  result=$?
+  if [[ $result -eq 
+  then
+    echo "Result successful"
+    break   
+  else
+    echo "Result unsuccessful"
+    sleep 1
+  fi
+done
+
+if [[ $result -ne 0 ]]
+then
+  echo "All of the trials failed!!!"
+fi
