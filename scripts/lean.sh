96ba9fb6b56714fbf31a5cfe78d6be10715d303c
0x676e67 <gngppz@gmail.com>
2023-01-14 13:41:24 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -10,7 +10,9 @@
 # Add cpufreq
 rm -rf ./feeds/luci/applications/luci-app-cpufreq 
 svn export https://github.com/DHDAXCW/luci-bt/trunk/applications/luci-app-cpufreq ./feeds/luci/applications/luci-app-cpufreq
+ls -lah ./feeds/luci/applications/luci-app-cpufreq/po/zh_Hans
 mv ./feeds/luci/applications/luci-app-cpufreq/po/zh_Hans ./feeds/luci/applications/luci-app-cpufreq/po/zh_cn
+ls -lah ./feeds/luci/applications/luci-app-cpufreq/po/zh_cn
 ln -sf ./feeds/luci/applications/luci-app-cpufreq ./package/feeds/luci/luci-app-cpufreq
 sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
 sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
