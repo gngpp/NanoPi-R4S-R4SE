a2ed191141a0dcad6321819d1b491a8a202b4d8f
gngpp <gngppz@gmail.com>
2023-01-22 15:52:01 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -92,7 +92,6 @@ git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add luci-theme
-# git clone https://github.com/DHDAXCW/theme
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth=1 https://github.com/gngpp/luci-theme-design
 git clone --depth=1 https://github.com/gngpp/luci-theme-atmaterial
@@ -137,9 +136,6 @@ popd
 
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-sed -i '/18.06/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
 popd
