44df6445a81c28c6563fba1dd1c7e206f63ca30f
gngpp <gngppz@gmail.com>
2023-01-27 21:01:57 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -12,7 +12,7 @@ pushd customfeeds
 
 # Add luci-app-onliner (need luci-app-nlbwmon)
 #git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
-svn export https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-onliner luci/applications/luci-app-onliner
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-onliner luci/applications/luci-app-onliner
 
 
 # Add luci-app-eqos
